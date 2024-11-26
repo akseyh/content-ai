d40899d4393dd02a84af102ffa8307c87267e9e8
akseyh <yilmazsems@gmail.com>
2024-11-26 21:11:23 +0300

    feat: adds auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@vee-validate/nuxt", "@prisma/nuxt"],
+  modules: [
+    "@nuxt/ui",
+    "@vee-validate/nuxt",
+    "@prisma/nuxt",
+    "@sidebase/nuxt-auth",
+  ],
+  auth: {
+    provider: {
+      type: "authjs",
+    },
+    baseURL: `http://localhost:3000`,
+  },
 });
---
+++ server/api/auth/[...].ts
@@ -0,0 +1,12 @@
+import GithubProvider from "next-auth/providers/github";
+import { NuxtAuthHandler } from "#auth";
+
+export default NuxtAuthHandler({
+  providers: [
+    // @ts-expect-error Use .default here for it to work during SSR.
+    GithubProvider.default({
+      clientId: process.env.GITHUB_PROVIDER_CLIENT_ID,
+      clientSecret: process.env.GITHUB_PROVIDER_CLIENT_SECRET,
+    }),
+  ],
+});
